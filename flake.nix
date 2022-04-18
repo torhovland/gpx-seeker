313ec9cdac86d7fd05f0172a86804b7910054449
Tor Hovland <tor+github@hovland.co>
2022-04-18 16:49:29 +0200

    Hello world with a Nix dev shell.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Will seek for a section of determined length and speed in a GPX file.";
+
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config.allowUnfree = true;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            (rust-bin.nightly.latest.default.override {
+              extensions = [ "rust-src" ];
+            })
+            vscode # In order for all Code Actions to work
+          ];
+
+          shellHook = ''
+            XDG_DATA_DIRS=$GSETTINGS_SCHEMA_PATH
+          '';
+        };
+      }
+    );
+}
