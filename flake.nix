9f4c544966467e440b1ae5bac6972598948512dc
Tor Hovland <tor+github@hovland.co>
2022-04-19 08:04:59 +0200

    cargo-flamegraph

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
               extensions = [ "rust-src" ];
             })
             cargo-edit
+            cargo-flamegraph
             cargo-watch
             vscode # In order for all Code Actions to work
           ];
